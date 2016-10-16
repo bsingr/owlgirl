5643891b7295467eff440413a701642bde72b99d
Jens Bissinger <mail@jens-bissinger.de>
2016-10-16 22:22:49 +0200

    Add motion control

--- app/index.js
+++ app/index.js
@@ -2,20 +2,59 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Owl from './owl';
 
+const keys = {
+  left: 37,
+  up: 38,
+  right: 39,
+  down: 40
+};
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {pos: 0}
+    this.state = {x: 0, y: 0, move: {x: 0, y: 0}};
   }
   componentDidMount() {
-    // setInterval(() => {
-    //   this.setState({pos: this.state.pos + 1})
-    // }, 10)
+    document.addEventListener("keyup", this.onKeyUp.bind(this), false);
+    document.addEventListener("keydown", this.onKeyDown.bind(this), false);
+    setInterval(() => {
+      this.setState({
+        x: this.state.x + this.state.move.x,
+        y: this.state.y + this.state.move.y,
+      });
+    }, 5);
+  }
+  onKeyDown(e) {
+    if (e.keyCode === keys.left) {
+      this.setState({move: Object.assign({}, this.state.move, {x: -1})});
+    } else if (e.keyCode === keys.up) {
+      this.setState({move: Object.assign({}, this.state.move, {y: -1})});
+    } else if (e.keyCode === keys.down) {
+      this.setState({move: Object.assign({}, this.state.move, {y: 1})});
+    } else if (e.keyCode === keys.right) {
+      this.setState({move: Object.assign({}, this.state.move, {x: 1})});
+    }
+  }
+  onKeyUp(e) {
+    if (e.keyCode === keys.left) {
+      this.setState({move: Object.assign({}, this.state.move, {x: 0})});
+    } else if (e.keyCode === keys.up) {
+      this.setState({move: Object.assign({}, this.state.move, {y: 0})});
+    } else if (e.keyCode === keys.down) {
+      this.setState({move: Object.assign({}, this.state.move, {y: 0})});
+    } else if (e.keyCode === keys.right) {
+      this.setState({move: Object.assign({}, this.state.move, {x: 0})});
+    }
   }
   render() {
-    return <div style={{position: 'absolute', top: this.state.pos}}>
-      <Owl />
-    </div>;
+    return (
+      <div
+        style={{
+          position: 'absolute', top: this.state.y, left: this.state.x
+        }}>
+        <Owl />
+      </div>
+    );
   }
 }
 

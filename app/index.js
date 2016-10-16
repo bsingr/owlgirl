f1d8d4f93e49762af40f077d4322cfda382a4c9b
Jens Bissinger <mail@jens-bissinger.de>
2016-10-16 20:57:38 +0200

    Add <Owl/>

--- app/index.js
+++ app/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Owl from './owl';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {pos: 0}
+  }
+  componentDidMount() {
+    // setInterval(() => {
+    //   this.setState({pos: this.state.pos + 1})
+    // }, 10)
+  }
   render() {
-    return <div>Hello</div>;
+    return <div style={{position: 'absolute', top: this.state.pos}}>
+      <Owl />
+    </div>;
   }
 }
 
